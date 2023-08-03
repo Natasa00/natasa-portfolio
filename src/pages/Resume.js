35b023a097bf327c3953fdc1792195bd5ca4fbf1
Natasa <nkilibarda3@gmail.com>
2023-08-03 16:32:15 +1000

    Add resume section

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,27 @@
+import ResumePdf from "../assets/resume.pdf";
+import "./Resume.css";
+
 export default function Resume() {
   return (
     <div>
       <h1 className="heading">RESUME</h1>
+
+      <a
+        href={ResumePdf}
+        className="resume-link"
+        download="Resume"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span className="icon">&#x1f4CE;</span> Download resume
+      </a>
+
+      <h2 className="resume-skills-heading">My skills</h2>
+      <ul className="resume-skills-list">
+        <li>HTML, CSS, JavaScript</li>
+        <li>ReactJS, NodeJS</li>
+        <li>MongoDB, MySQL</li>
+      </ul>
     </div>
   );
 }
